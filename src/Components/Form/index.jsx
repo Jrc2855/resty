c44485c17a6bf389dc6c5fb29718ab06b1f400ad
John Chavez <Jrc2855@gmail.com>
2023-04-16 23:32:31 +0000

    Lab 29

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,73 +1,170 @@
-import React, { useState, useReducer } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import './Form.scss';
 
-export const initialState = {
-  name: 'Search Results',
-  searchHistory: ['abc.com', 'xyz.com'],
-};
 
-export const searchReducer = (state = initialState, action) => {
-  switch(action.type) {
-    case 'SEARCH':
-      return { ...state, searchHistory: [...state.searchHistory, action.payload] }
-    case 'REMOVE':
-      return { ...state, searchHistory: state.searchHistory.filter(search => search !== action.payload) };
-    default:
-      return state;
-  }
-};
+const Form = (props) => {
+  
+  const [method, setMethod] = useState('get');
+  const [url, setUrl] = useState('');
+  const [data, setData] = useState('');
 
-const Search = () => {
-  const [input, setInput] = useState('');
-  const [quote, setQuote] = useState('');
-  const [state, dispatch] = useReducer(searchReducer, initialState);
-
-  const addSearch = () => {
-    let action = {
-      type:'SEARCH',
-      payload: input,
-    };
-    dispatch(action);
-  }
-  const removeSearch = () => {
-    let action = {
-      type: 'REMOVE',
-      payload: input,
-    };
-    dispatch(action);
-  };
-
-  const axiosQuote = () => {
-    axios.get('https://api.quotable.io/random')
-    .then(response => {
-      console.log(response.data.content)
-      setQuote(response.data.content)
-    }).catch(error => {
-      console.log(error);
-    })
+  const handleSubmit = e => {
+    e.preventDefault();
+    const formData = {
+      method, url, data,
+    }
+    props.handleApiCall(formData);
   }
 
-
-  return(
+  return (
     <>
-      <h1>{state.name}</h1>
-      <input onChange={(e) => setInput(e.target.value)} />
-      <button onClick={addSearch}>Search for a URL</button>
-      <button onClick={removeSearch}>Delete A Search Result</button>
-      <button onClick={axiosQuote}>Click here to generate a Motivational Quote</button>
-      <h3>{quote && quote}</h3>
-
-    <ul>
-      {
-        state.searchHistory.map((search, idx) => (
-          <li key={`search=${idx}`}>{search}</li>
-        ))
-      }
-    </ul>
-
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL:</span>
+          <input name='url' type='text' onChange={(e) => setUrl(e.target.value)} />
+          <button type='submit'>Submit</button>
+        </label>
+        <label>
+          <textarea rows="4" cols="50" onChange={(e) => setData(e.target.value)} />
+        </label>
+        <label className="methods">
+          <span
+            onClick={(e) => setMethod(e.target.id)}
+          >READ
+          </span>
+          <span
+            onClick={(e) => setMethod(e.target.id)}
+          >CREATE
+          </span>
+          <span
+            onClick={(e) => setMethod(e.target.id)}
+          >UPDATE
+          </span>
+          <span
+            onClick={(e) => setMethod(e.target.id)}
+          >
+          DELETE
+          </span>
+        </label>
+      </form>
     </>
   )
 }
+export default Form;
 
-export default Search;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----Legacy Code-----//
+// export const initialState = {
+//   name: 'Search Results',
+//   searchHistory: ['abc.com', 'xyz.com'],
+// };
+
+// export const searchReducer = (state = initialState, action) => {
+//   switch(action.type) {
+//     case 'SEARCH':
+//       return { ...state, searchHistory: [...state.searchHistory, action.payload] }
+//     case 'REMOVE':
+//       return { ...state, searchHistory: state.searchHistory.filter(search => search !== action.payload) };
+//     default:
+//       return state;
+//   }
+// };
+
+// const Search = () => {
+//   const [input, setInput] = useState('');
+//   const [quote, setQuote] = useState('');
+//   const [state, dispatch] = useReducer(searchReducer, initialState);
+
+//   const addSearch = () => {
+//     let action = {
+//       type:'SEARCH',
+//       payload: input,
+//     };
+//     dispatch(action);
+//   }
+//   const removeSearch = () => {
+//     let action = {
+//       type: 'REMOVE',
+//       payload: input,
+//     };
+//     dispatch(action);
+//   };
+
+
+//   const axiosQuote = () => {
+//     axios.get('https://api.quotable.io/random')
+//     .then(response => {
+//       console.log(response.data.content)
+//       setQuote(response.data.content)
+//     }).catch(error => {
+//       console.log(error);
+//     })
+//   }
+
+
+//   return(
+//     <>
+//       <h1>{state.name}</h1>
+//       <input onChange={(e) => setInput(e.target.value)} />
+//       <button onClick={addSearch}>Search for a URL</button>
+//       <button onClick={removeSearch}>Delete A Search Result</button>
+//       <button onClick={axiosQuote}>Click here to generate a Motivational Quote</button>
+//       <h3>{quote && quote}</h3>
+
+//     <ul>
+//       {
+//         state.searchHistory.map((search, idx) => (
+//           <li key={`search=${idx}`}>{search}</li>
+//         ))
+//       }
+//     </ul>
+
+//     </>
+//   )
+// }
+
+// export default Search;
